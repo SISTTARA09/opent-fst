b075c77f4f7c7a1af08b2f4720baf7020d5e4d79
suitablePanther <suitablepanther@gmail.com>
2024-03-21 11:26:52 +0000

    DEBUG: - use omit instead of "same origin" in fetch request credentials

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -30,7 +30,7 @@ const AuthContext = ({ children }: { children: React.ReactElement }) => {
 					method: "GET",
 					headers: {
 						Authorization: `bearer ${token}`,
-						credentials: "same-origin",
+						credentials: "omit",
 					},
 				});
 				const user = await response.json();
