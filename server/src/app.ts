aa046c3dd23765208de941338c98c45080d4c1c8
suitablePanther <suitablepanther@gmail.com>
2024-03-21 11:46:15 +0000

    DEBUG: - remove cors package and configure manualy

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,12 +15,16 @@ const app: express.Application = express();
 ///
 
 // middlewares
-app.use(
-	cors({
-		origin: process.env.NODE_MAILER_CLIENT_ADRESS,
-		credentials: true,
-	})
-);
+app.use(function (_req, res, next) {
+	res.setHeader(
+		"Access-Control-Allow-Origin",
+		"https://your-client-domain.com"
+	);
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
